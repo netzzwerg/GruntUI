35cfdc86bb80110a2078526e0e186359efc61208
Béla Varga <netzzwerg@googlemail.com>
2014-07-04 00:31:23 +0200

    Added routing.

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,16 @@
 'use strict';
 
-var gruntUIApp = angular.module('gruntUIApp', [])
+var gruntUIApp = angular.module('gruntUIApp', ['ngRoute'])
   .controller('NavigationItemsCtrl', function($scope, $http){
     $http.get('app/json/navigationItems.json').then(function(navigationItemsResponse) {
       $scope.navigationItems = navigationItemsResponse.data;
     });
-  });
+  })
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/overview', { templateUrl: 'app/html/view-overview.html' })
+      .when('/tasks', { templateUrl: 'app/html/view-tasks.html' })
+      .when('/components', { templateUrl: 'app/html/view-components.html' })
+      .when('/modules', { templateUrl: 'app/html/view-modules.html' })
+      .otherwise({ redirectTo: '/' });
+});
